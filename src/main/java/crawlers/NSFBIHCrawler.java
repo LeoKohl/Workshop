7054621fb0f6488a28a1adda9e50f2d4dfb97036
Mikami-Reo <le.kohl@gmx.net>
2024-09-18 19:01:50 +0200

    fix URL duplication

--- src/main/java/crawlers/NSFBIHCrawler.java
+++ src/main/java/crawlers/NSFBIHCrawler.java
@@ -11,13 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-
-/* TODO
- *   reduce duplication in URL strings
- *   make category not a global field
- *   make download use session & newRequest*/
-
-
 public class NSFBIHCrawler extends CrawlerInterface {
 
     private static final String timeZone = "Europe/Sarajevo";
@@ -27,24 +20,26 @@ public class NSFBIHCrawler extends CrawlerInterface {
     private static final String dateFormat = "dd.MM.yyyy. HH:mm";
 
     protected static String CATEGORY;
+    
+    private static final String BASE_URL = "https://www.nsfbih.ba/takmicenja/";
 
     private static final Map<String, String> league_Map = Map.ofEntries(
-            Map.entry("WWIN.Prva.Liga.FBiH", "https://www.nsfbih.ba/takmicenja/wwin-prva-liga-fbih"),
-            Map.entry("WWIN.Prva.Liga.FBiH-Sjever", "https://www.nsfbih.ba/takmicenja/wwin-druga-liga-fbih-sjever"),
-            Map.entry("WWIN.Prva.Liga.FBiH-Centar", "https://www.nsfbih.ba/takmicenja/wwin-druga-liga-fbih-centar"),
-            Map.entry("WWIN.Prva.Liga.FBiH-Zapad", "https://www.nsfbih.ba/takmicenja/wwin-druga-liga-fbih-zapad"),
-            Map.entry("WWIN.Prva.Liga.FBiH-Jug", "https://www.nsfbih.ba/takmicenja/wwin-druga-liga-fbih-jug"),
-            Map.entry("Prva.ženska.Liga.FBiH","https://www.nsfbih.ba/takmicenja/prva-zenska-liga-fbih"),
-            Map.entry("Omladinska.Liga.BiH.Kadeti-Centar.1","https://www.nsfbih.ba/takmicenja/omladinska-liga-bih-kadeti-centar-1"),
-            Map.entry("Omladinska.Liga.BiH.Juniori-Centar.1", "https://www.nsfbih.ba/takmicenja/omladinska-liga-bih-juniori-centar-1"),
-            Map.entry("Omladinska.Liga.BiH.Kadeti-Centar.2","https://www.nsfbih.ba/takmicenja/omladinska-liga-bih-kadeti-centar-2"),
-            Map.entry("Omladinska.Liga.BiH.Juniori-Centar.2", "https://www.nsfbih.ba/takmicenja/omladinska-liga-bih-juniori-centar-2"),
-            Map.entry("Omladinska.Liga.BiH.Kadeti-Jug","https://www.nsfbih.ba/takmicenja/omladinska-liga-bih-kadeti-jug"),
-            Map.entry("Omladinska.Liga.BiH.Juniori-Jug", "https://www.nsfbih.ba/takmicenja/omladinska-liga-bih-juniori-jug"),
-            Map.entry("Prva.Futsal.liga.FBiH-Sjever","https://www.nsfbih.ba/takmicenja/prva-futsal-liga-fbih-sjever"),
-            Map.entry("Prva.Futsal.liga.FBiH-Centar","https://www.nsfbih.ba/takmicenja/prva-futsal-liga-fbih-centar"),
-            Map.entry("Prva.Futsal.liga.FBiH-Zapad","https://www.nsfbih.ba/takmicenja/prva-futsal-liga-fbih-zapad"),
-            Map.entry("Prva.Futsal.liga.FBiH-Jug","https://www.nsfbih.ba/takmicenja/prva-futsal-liga-fbih-jug")
+            Map.entry("WWIN.Prva.Liga.FBiH", "wwin-prva-liga-fbih"),
+            Map.entry("WWIN.Prva.Liga.FBiH-Sjever", "wwin-druga-liga-fbih-sjever"),
+            Map.entry("WWIN.Prva.Liga.FBiH-Centar", "wwin-druga-liga-fbih-centar"),
+            Map.entry("WWIN.Prva.Liga.FBiH-Zapad", "wwin-druga-liga-fbih-zapad"),
+            Map.entry("WWIN.Prva.Liga.FBiH-Jug", "wwin-druga-liga-fbih-jug"),
+            Map.entry("Prva.ženska.Liga.FBiH","prva-zenska-liga-fbih"),
+            Map.entry("Omladinska.Liga.BiH.Kadeti-Centar.1","omladinska-liga-bih-kadeti-centar-1"),
+            Map.entry("Omladinska.Liga.BiH.Juniori-Centar.1", "omladinska-liga-bih-juniori-centar-1"),
+            Map.entry("Omladinska.Liga.BiH.Kadeti-Centar.2","omladinska-liga-bih-kadeti-centar-2"),
+            Map.entry("Omladinska.Liga.BiH.Juniori-Centar.2", "omladinska-liga-bih-juniori-centar-2"),
+            Map.entry("Omladinska.Liga.BiH.Kadeti-Jug","omladinska-liga-bih-kadeti-jug"),
+            Map.entry("Omladinska.Liga.BiH.Juniori-Jug", "omladinska-liga-bih-juniori-jug"),
+            Map.entry("Prva.Futsal.liga.FBiH-Sjever","prva-futsal-liga-fbih-sjever"),
+            Map.entry("Prva.Futsal.liga.FBiH-Centar","prva-futsal-liga-fbih-centar"),
+            Map.entry("Prva.Futsal.liga.FBiH-Zapad","prva-futsal-liga-fbih-zapad"),
+            Map.entry("Prva.Futsal.liga.FBiH-Jug","prva-futsal-liga-fbih-jug")
     );
 
     @Override
@@ -52,7 +47,7 @@ public class NSFBIHCrawler extends CrawlerInterface {
         league_Map.forEach((category, url) -> {
             CATEGORY = category;
             try {
-                Document document = getDocument(url);
+                Document document = getDocument(BASE_URL+url);
                 if (document != null) {
                     parseDocument(document);
                 }
